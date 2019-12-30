fb33cad118545e161628abbad84e3c290123bcc9
Girish <bharath.giri@me.com>
2019-12-30 16:41:05 +0530

    adding example microservice

---
+++ Spring-boot-example/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.in28minutes.springboot.microservice.example.forex")
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+
+}
