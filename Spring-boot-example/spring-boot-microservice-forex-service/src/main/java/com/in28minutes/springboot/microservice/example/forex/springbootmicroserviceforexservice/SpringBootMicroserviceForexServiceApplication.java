ad5c65583ed9bdb3ad7d0f0b5835d4997eabcbef
Girish <bharath.giri@me.com>
2019-12-31 11:59:39 +0530

    dockerised 3 services

--- Spring-boot-example/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
+++ Spring-boot-example/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
@@ -2,10 +2,14 @@ package com.in28minutes.springboot.microservice.example.forex.springbootmicroser
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.in28minutes.springboot.microservice.example.forex")
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringBootMicroserviceForexServiceApplication {
 
 	public static void main(String[] args) {
